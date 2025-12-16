782d9700c3e58bed6015d33a823b902dea35e89c
sakamoto-kanna <kimgmg9136@gmail.com>
2025-12-16 12:17:40 +0900

    footer layout add

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,3 +1,16 @@
 export default function Footer() {
-  return <footer>푸터자리</footer>;
+  return (
+    <footer>
+      <div className="bg-gray-700 text-white flex p-10 space-x-96 justify-center items-center">
+        <img src="images/logo.svg" alt="" />
+        <div>
+          <p>푸터 정보</p>
+          <p>© 2024 다잇슈대전. All rights reserved.</p>
+        </div>
+        <div>
+          <p>inst face ..</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
