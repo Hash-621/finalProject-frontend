b18a5b2f45b4d50d6e9d1a01c2c9e0e1bb132a48
Soomin Kim <sumink4818@gmail.com>
2025-12-31 09:22:14 +0900

    Revert "12-31"
    This reverts commit b985e313694d79522b9a95de446fcc1a1e72f70f.

--- src/components/layouts/Container.tsx
+++
@@ -1,9 +0,0 @@
-export default function Container({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <div className="relative overflow-hidden lg:min-w-7xl">{children}</div>
-  );
-}
--- src/types/user.ts
+++
@@ -1,4 +0,0 @@
-export interface UserData {
-  loginId: string;
-  password: string;
-}
