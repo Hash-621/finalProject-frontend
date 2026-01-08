ffe28ad57821c181cbfeb88cf3a9c38c8d6bc1d6
sakamoto-kanna <kimgmg9136@gmail.com>
2026-01-08 12:40:44 +0900

    꼬인거 수정...

--- src/hooks/useAdminCheck.ts
+++ src/hooks/useAdminCheck.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { fetchClient } from "@/utils/api";
+import { userService } from "@/api/services";
 
 interface UserData {
   userId?: number | string;
@@ -9,6 +10,8 @@ interface UserData {
   role: string;
   [key: string]: any;
 }
+
+const serverURL = process.env.NEXT_PUBLIC_API_URL;
 
 export default function useAdminCheck() {
   const router = useRouter();
@@ -20,11 +23,23 @@ export default function useAdminCheck() {
   useEffect(() => {
     const checkAuth = async () => {
       try {
+        const userInfo = await userService.getUserInfo();
         const data = await fetchClient("/api/v1/user/auth");
+        const response = await fetch(`${serverURL}/api/v1/admin/isAdmin`, {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ loginId: userInfo.data.loginId }),
+        });
+        const userIsAdmin = await response.json();
+        if (response.ok && userIsAdmin) {
+          setIsAdmin(true);
+          return;
+        }
 
         if (data) {
           setUserData(data);
-
           if (data.role === "ROLE_ADMIN") {
             setIsAdmin(true);
           } else {
