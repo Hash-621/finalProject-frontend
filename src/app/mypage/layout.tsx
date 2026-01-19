199f82ed33a5628aac12d1cdfbd95b27e2233547
Hansh <hansh0621@naver.com>
2026-01-19 13:52:37 +0900

    최종최종최종최종

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -20,7 +20,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   // 4. 브라우저 탭에 표시될 페이지 제목을 설정합니다.
   // 사용자가 이 레이아웃이 적용된 페이지(마이페이지)에 접속하면, 탭 제목이 "NextShop | My-Page"로 바뀝니다.
-  title: "NextShop | My-Page",
+  title: "다잇슈대전 | My-Page",
 };
 
 // --- [레이아웃 컴포넌트 정의] ---
